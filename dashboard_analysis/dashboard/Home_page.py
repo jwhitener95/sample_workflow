30e38a69d648b51b8bba1ceb7eab8e87ad4c766a
jwhitener95 <43548155+jwhitener95@users.noreply.github.com>
2024-10-01 16:01:54 +0000

    Update dashboard

--- dashboard_analysis/dashboard/Home_page.py
+++ dashboard_analysis/dashboard/Home_page.py
@@ -7,9 +7,15 @@ st.write('---')
 
 st.write('⚠️ The current dashboard is in construction ⚠️')
 st.write('---')
-st.write('''
-         The following datasets will be used to demonstrate the workflow \n
-         Miller YE, Ghosh M, Merrick DT, Kubala B et al. Phase Ib trial of inhaled iloprost for the prevention of lung cancer with predictive and response biomarker assessment. Front Oncol 2023;13:1204726. PMID: 37711198 \n
-         Wang D, Li S, Yang Z, Yu C et al. Single-cell transcriptome analysis deciphers the CD74-mediated immune evasion and tumour growth in lung squamous cell carcinoma with chronic obstructive pulmonary disease. Clin Transl Med 2024 Aug;14(8):e1786. PMID: 39113235
-         ''')
+url_Miller = 'https://pubmed.ncbi.nlm.nih.gov/37711198/'
+url_Wang = 'https://pubmed.ncbi.nlm.nih.gov/39113235/'
+st.write('**The following datasets will be used to demonstrate the workflow**')
+st.write('Miller YE, Ghosh M, Merrick DT, Kubala B et al. Phase Ib trial of inhaled iloprost for the prevention of lung cancer with predictive and response biomarker assessment. Front Oncol 2023;13:1204726. PMID: [37711198](%s)' % url_Miller)
+st.write('Wang D, Li S, Yang Z, Yu C et al. Single-cell transcriptome analysis deciphers the CD74-mediated immune evasion and tumour growth in lung squamous cell carcinoma with chronic obstructive pulmonary disease. Clin Transl Med 2024 Aug;14(8):e1786. PMID: [39113235](%s)' % url_Wang)
 
+st.write('---')
+st.subheader('⇦ Select a page in the sidebar to begin')
+st.write('---')
+
+git_link = 'https://github.com/jwhitener95/sample_workflow'
+st.write('Visit the [GitHub page](%s) for further details' % git_link)
