49b57fd77fe5fe53a9fbcff0612db14819dd0bd7
jwhitener0 <jwhitener95@gmail.com>
2024-10-18 15:48:44 -0400

    Add results

--- IntegrationFunctions.py
+++ IntegrationFunctions.py
@@ -148,3 +148,57 @@ def integrate_datasets(adata):
     sc.tl.leiden(adata)
     sc.tl.umap(adata, method=method)
     return (adata, model)
+
+def save_umaps(adata):
+    """
+    Save plots for all available marker genes.
+    """
+
+    # Get marker genes
+    marker_genes = {
+        "CD14+ Mono": ["FCN1", "CD14"],
+        "CD16+ Mono": ["TCF7L2", "FCGR3A", "LYN"],
+        # Note: DMXL2 should be negative
+        "cDC2": ["CST3", "COTL1", "LYZ", "DMXL2", "CLEC10A", "FCER1A"],
+        "Erythroblast": ["MKI67", "HBA1", "HBB"],
+        # Note HBM and GYPA are negative markers
+        "Proerythroblast": ["CDK6", "SYNGR1", "HBM", "GYPA"],
+        "NK": ["GNLY", "NKG7", "CD247", "FCER1G", "TYROBP", "KLRG1", "FCGR3A"],
+        "ILC": ["ID2", "PLCG2", "GNLY", "SYNE1"],
+        "Naive CD20+ B": ["MS4A1", "IL4R", "IGHD", "FCRL1", "IGHM"],
+        # Note IGHD and IGHM are negative markers
+        "B cells": [
+            "MS4A1",
+            "ITGB1",
+            "COL4A4",
+            "PRDM1",
+            "IRF4",
+            "PAX5",
+            "BCL11A",
+            "BLK",
+            "IGHD",
+            "IGHM",
+        ],
+        "Plasma cells": ["MZB1", "HSP90B1", "FNDC3B", "PRDM1", "IGKC", "JCHAIN"],
+        # Note PAX5 is a negative marker
+        "Plasmablast": ["XBP1", "PRDM1", "PAX5"],
+        "CD4+ T": ["CD4", "IL7R", "TRBC2"],
+        "CD8+ T": ["CD8A", "CD8B", "GZMK", "GZMA", "CCL5", "GZMB", "GZMH", "GZMA"],
+        "T naive": ["LEF1", "CCR7", "TCF7"],
+        "pDC": ["GZMB", "IL3RA", "COBLL1", "TCF4"],
+    }
+    celltypes = list(marker_genes.keys())
+    genes = list(marker_genes.values())
+
+    # Save plots
+    save_as = 'D:/GitHub/sample_workflow/process_datasets/figures/'
+    sc._settings.ScanpyConfig(figdir=save_as)
+
+    sc.pl.umap(adata, color="cell_type", frameon=False, size=3, legend_fontsize='xx-small', legend_fontweight='light', 
+            legend_fontoutline=2, save=f'_integrated.png', show=False)
+
+    for i in range(0, len(celltypes)):
+        for gene in genes[i]:
+            if gene in adata.var_names:
+                sc.pl.umap(adata, color=gene, frameon=False, size=3, legend_fontsize='xx-small', legend_fontweight='light', 
+                        legend_fontoutline=2, save=f'_integrated_{celltypes[i]}_{gene}.png', show=False)
